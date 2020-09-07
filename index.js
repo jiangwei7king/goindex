b5d5c93ebd0fb1660e8167e21f04f10e6ae120f0
jiangwei7king <65028365+jiangwei7king@users.noreply.github.com>
2020-09-08 00:11:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// https://github.com/donwa/goindex/blob/master/使用及免责协议.md
-
 var authConfig = {
     "siteName": "GoIndex", // 网站名称
     "root_pass": "index",  // 根目录密码，优先于.password
@@ -20,7 +18,7 @@ var html = `
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
   <title>${authConfig.siteName}</title>
-  <script src="//cdn.jsdelivr.net/combine/gh/jquery/jquery@3.2/dist/jquery.min.js,gh/jiangwei7king/goindex@${authConfig.version}/themes/${authConfig.theme}/app.js"></script>
+  <script src="//cdn.jsdelivr.net/combine/gh/jquery/jquery@3.2/dist/jquery.min.js,gh/jiangwei7king/goindex/themes/${authConfig.theme}/app.js"></script>
 </head>
 <body>
 </body>
@@ -48,32 +46,13 @@ async function handleRequest(request) {
     let path = url.pathname;
     let action = url.searchParams.get('a');
 
-    if(path.substr(-1) == '/'){
-      try {
-        await gd.list(path);
-      } catch (e) {
-        return new Response("", { status: 404 }); // if path: /notexist/
-      }
-      return new Response(html, { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' } });
-    } else if(action != null){
-      if (await gd.file(path) == undefined){
-        return new Response(html404, { status: 404, headers: { 'Content-Type': 'text/html; charset=utf-8' } });
-      }
-      return new Response(html, { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' } });
-    } else {
-      if (path.split('/').pop().toLowerCase() == ".password") {
-        return new Response("", { status: 404 });
-      }
-      try {
-        await gd.file(path);
-      } catch (e) {
-        return new Response("", { status: 404 }); // if path: /notexist/notexist
+    if(path.substr(-1) == '/' || action != null){
+      return new Response(html,{status:200,headers:{'Content-Type':'text/html; charset=utf-8'}});
+    }else{
+      if(path.split('/').pop().toLowerCase() == ".password"){
+         return new Response("",{status:404});
       }
       let file = await gd.file(path);
-      if (file == undefined){
-        return new Response("", { status: 404 }); // if path: /exist/notexist
-      }
-      
       let range = request.headers.get('Range');
       return gd.down(file.id, range);
     }
@@ -97,7 +76,7 @@ async function apiRequest(request) {
           var obj = {};
         }
         console.log(password,obj);
-        if(password.replace("\n", "") != obj.password){
+        if(password != obj.password){
           let html = `{"error": {"code": 401,"message": "password error."}}`;
           return new Response(html,option);
         }
@@ -149,36 +128,19 @@ class googleDrive {
       let url = 'https://www.googleapis.com/drive/v3/files';
       let params = {'includeItemsFromAllDrives':true,'supportsAllDrives':true};
       params.q = `'${parent}' in parents and name = '${name}' andtrashed = false`;
-      params.fields = "files(id, name, mimeType, size ,createdTime, modifiedTime, iconLink, thumbnailLink, shortcutDetails)";
+      params.fields = "files(id, name, mimeType, size ,createdTime, modifiedTime, iconLink, thumbnailLink)";
       url += '?'+this.enQuery(params);
       let requestOption = await this.requestOption();
       let response = await fetch(url, requestOption);
       let obj = await response.json();
-      if (obj.files && obj.files[0] && obj.files[0].mimeType == 'application/vnd.google-apps.shortcut'){
-        obj.files[0].id = obj.files[0].shortcutDetails.targetId;
-        obj.files[0].mimeType = obj.files[0].shortcutDetails.targetMimeType;
-      }
       console.log(obj);
       return obj.files[0];
     }
 
     // 通过reqeust cache 来缓存
     async list(path){
-      if (gd.cache == undefined) {
-        gd.cache = {};
-      }
-
-      if (gd.cache[path]) {
-        return gd.cache[path];
-      }
-
       let id = await this.findPathId(path);
-      var obj = await this._ls(id);
-      if (obj.files && obj.files.length > 1000) {
-            gd.cache[path] = obj;
-      }
-
-      return obj
+      return this._ls(id);
     }
 
     async password(path){
@@ -207,35 +169,16 @@ class googleDrive {
       if(parent==undefined){
         return null;
       }
-      const files = [];
-      let pageToken;
-      let obj;
+      let url = 'https://www.googleapis.com/drive/v3/files';
       let params = {'includeItemsFromAllDrives':true,'supportsAllDrives':true};
       params.q = `'${parent}' in parents and trashed = false AND name !='.password'`;
       params.orderBy= 'folder,name,modifiedTime desc';
-      params.fields = "nextPageToken, files(id, name, mimeType, size , modifiedTime, shortcutDetails)";
+      params.fields = "nextPageToken, files(id, name, mimeType, size , modifiedTime)";
       params.pageSize = 1000;
-
-      do {
-        if (pageToken) {
-            params.pageToken = pageToken;
-        }
-        let url = 'https://www.googleapis.com/drive/v3/files';
-        url += '?'+this.enQuery(params);
-        let requestOption = await this.requestOption();
-        let response = await fetch(url, requestOption);
-        obj = await response.json();
-        obj.files.forEach(file => {
-          if (file && file.mimeType == 'application/vnd.google-apps.shortcut') {
-            file.id = file.shortcutDetails.targetId;
-            file.mimeType = file.shortcutDetails.targetMimeType;
-          }
-        });
-        files.push(...obj.files);
-        pageToken = obj.nextPageToken;
-      } while (pageToken);
-
-      obj.files = files;
+      url += '?'+this.enQuery(params);
+      let requestOption = await this.requestOption();
+      let response = await fetch(url, requestOption);
+      let obj = await response.json();
       return obj;
     }
 
@@ -272,18 +215,13 @@ class googleDrive {
 
       let url = 'https://www.googleapis.com/drive/v3/files';
       let params = {'includeItemsFromAllDrives':true,'supportsAllDrives':true};
-      params.q = `'${parent}' in parents and (mimeType = 'application/vnd.google-apps.folder' or mimeType = 'application/vnd.google-apps.shortcut') and name = '${name}'  and trashed = false`;
-      params.fields = "nextPageToken, files(id, name, mimeType, shortcutDetails)";
+      params.q = `'${parent}' in parents and mimeType = 'application/vnd.google-apps.folder' and name = '${name}'  and trashed = false`;
+      params.fields = "nextPageToken, files(id, name, mimeType)";
       url += '?'+this.enQuery(params);
       let requestOption = await this.requestOption();
       let response = await fetch(url, requestOption);
       let obj = await response.json();
       if(obj.files[0] == undefined){
-        return null;
-      }
-      if (obj.files[0].mimeType == 'application/vnd.google-apps.shortcut' && obj.files[0].shortcutDetails.targetMimeType == 'application/vnd.google-apps.folder') {
-        obj.files[0].id = obj.files[0].shortcutDetails.targetId;
-      } else if (obj.files[0].mimeType == 'application/vnd.google-apps.shortcut' && obj.files[0].shortcutDetails.targetMimeType != 'application/vnd.google-apps.folder'){
         return null;
       }
       return obj.files[0].id;
